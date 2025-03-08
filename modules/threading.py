11ed15257610d5df2c7db1b2599443c9d5fcca80
berkaygediz <121058050+berkaygediz@users.noreply.github.com>
2025-03-08 06:02:44 +0300

    unnecessary comments

--- modules/threading.py
+++ modules/threading.py
@@ -9,12 +9,12 @@ class ThreadingEngine(QThread):
     def __init__(self, adaptiveResponse: float, parent=None):
         super(ThreadingEngine, self).__init__(parent)
         self.adaptiveResponse = adaptiveResponse
-        self.running = False  # Mutex kullanımı gereksiz
+        self.running = False
 
     def run(self):
         if self.running:
-            return  # Zaten çalışıyorsa tekrar çalıştırma
+            return
         self.running = True
         time.sleep(0.15 * self.adaptiveResponse)
         self.update.emit()
-        self.running = False  # Thread tamamlandıktan sonra sıfırla
+        self.running = False
